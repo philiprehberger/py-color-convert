e608b50a6e359947096de99881bbb796c12357dd
Philip Rehberger <me@philiprehberger.com>
2021-06-10 16:09:08 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_color_convert
+    assert hasattr(philiprehberger_color_convert, "__name__") or True
